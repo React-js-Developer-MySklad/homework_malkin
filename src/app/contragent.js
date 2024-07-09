b30a0b0a850a6385f8f495923dd82257defa3244
dmalkin <dmalkin@moysklad.ru>
2024-07-09 22:49:17 +0400

    конфиги удаление старого

--- src/app/contragent.js
+++
@@ -1,9 +0,0 @@
-export class Contragent {
-    constructor(id, name, itn, address, trrc) {
-        this.id = id;
-        this.name = name;
-        this.itn = itn;
-        this.address = address;
-        this.trrc = trrc;
-    }
-}
