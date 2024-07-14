d041a344092a385b5fc52d74ffd38f1bc079c5b8
dmalkin <dmalkin@moysklad.ru>
2024-07-14 18:47:21 +0400

    попраивл конфиги

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './node_modules/flowbite/**/*.js'
+    // ...
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
-    require('flowbite/plugin'),
-  ]
-}
+    // ...
+    flowbite.plugin(),
+  ],
+};
 
